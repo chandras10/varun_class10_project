448a1d7e61f9752c16773cc8cbc43663d15b98bc
chandras10 <chandras10>
2018-11-06 15:37:14 +0530

    Vinayaks Caluculator and snakes and ladders added

---
+++ demo/core/SnakeNLadderTest.java
@@ -0,0 +1,19 @@
+package demo.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+public class SnakeNLadderTest {
+
+	public static void main(String[] args) {
+		SnakeNLadder s = new SnakeNLadder();
+		s.startGame();
+
+	}
+
+}
+
