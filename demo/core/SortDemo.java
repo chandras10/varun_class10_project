cffeec4dabc14369d77c27a98d0c5557c0895285
chandras10 <chandras10>
2018-11-15 14:52:24 +0530

    almost final animation of the sorter

--- demo/core/SortDemo.java
+++ demo/core/SortDemo.java
@@ -3,12 +3,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 public class SortDemo implements ActionListener{
-    
     private int arraySize;
     private int sortAlgoType;
-    
+
     private JFrame mainframe;
     private JLabel arraySizeLbl;
     private JLabel sortAlgoTypeLbl;
@@ -16,14 +14,14 @@ public class SortDemo implements ActionListener{
     private JButton sortButton;
     private JComboBox sortAlgoList;
     private JTextField arraySizeTxtField;
-    
+
     public SortDemo() {
-        
+
         mainframe = new JFrame("Sorting Demo");
         mainframe.setVisible(true);
         mainframe.setSize(700, 600);
         mainframe.setLayout(new GridBagLayout());
-        
+
         arraySizeLbl = new JLabel("Size of the Array: ", JLabel.RIGHT);
         GridBagConstraints c = new GridBagConstraints();
         c.weightx = 0.5;
@@ -32,29 +30,29 @@ public class SortDemo implements ActionListener{
         c.gridy = 0;
         c.insets = new Insets(20, 5, 5, 5);
         mainframe.add(arraySizeLbl, c);
-        
+
         arraySizeTxtField = new JTextField(3);
         c.weightx = 0.5;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
         c.insets = new Insets(20, 5, 5, 500);
-        
+
         mainframe.add(arraySizeTxtField, c);
-        
+
         sortAlgoTypeLbl = new JLabel("Type of Sort: ", JLabel.RIGHT);
         c.weightx = 0.5;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 1;
         c.insets = new Insets(20, 5, 20, 5);
-        
+
         mainframe.add(sortAlgoTypeLbl, c);
-        
+
         final DefaultComboBoxModel sortAlgoListModel = new DefaultComboBoxModel();
         sortAlgoListModel.addElement("Bubble Sort");
         sortAlgoListModel.addElement("Selection Sort");
-    
+
         sortAlgoList = new JComboBox(sortAlgoListModel);    
         sortAlgoList.setSelectedIndex(0);
         c.weightx = 0.5;
@@ -63,7 +61,7 @@ public class SortDemo implements ActionListener{
         c.gridy = 1;
         c.insets = new Insets(20, 5, 20, 500);
         mainframe.add(sortAlgoList, c);
-        
+
         sortButton = new JButton("Sort");
         sortButton.addActionListener(this);
         GridBagConstraints cb = new GridBagConstraints();
@@ -74,34 +72,48 @@ public class SortDemo implements ActionListener{
         cb.gridwidth = 2;
         cb.insets = new Insets(20, 5, 20, 5);
         mainframe.add(sortButton, cb);
-        
+
         mainframe.setAlwaysOnTop (true);
     }
-    
+
     public void actionPerformed(ActionEvent event) {
-        sortButton.setText("Sorting...");
+        boolean flag = true;
         int n = Integer.parseInt(arraySizeTxtField.getText());
+
+        while (flag == true) {
+            n = Integer.parseInt(arraySizeTxtField.getText());
+
+            if(n > 500) {
+                System.out.println("enter a value below 500...");
+                arraySizeTxtField.setText(null);
+                arraySizeTxtField.requestFocus();    
+                sortButton.requestFocus();
+                continue;
+            }
+            flag = false;
+        } // end while
+        sortButton.setText("Sorting...");        
         int choice = sortAlgoList.getSelectedIndex();
         SortArray A = new SortArray(n);
         SortAlgorithm algo = null;
-        
+
         switch (choice) {
-                
-                case 0:
-                    algo = new BubbleSort();
-                    break;
-                case 1:
-                    algo = new SelectionSort();
-                    break;
-                default:
-                    System.out.println("Wrong choice");
-                    System.out.println();
-            } // end switch
-        
+
+            case 0:
+            algo = new BubbleSort();
+            break;
+            case 1:
+            algo = new SelectionSort();
+            break;
+            default:
+            System.out.println("Wrong choice");
+            System.out.println();
+        } // end switch
+
         /*A.shuffle();
         System.out.print("The original array is: ");
         A.print();
-        
+
         System.out.println("Algo used for sorting: " + algo.getName());
         algo.sort(A);
         System.out.println();
@@ -111,11 +123,11 @@ public class SortDemo implements ActionListener{
         System.out.println("\n");
         sortButton.setText("Sort");*/
         SimpleGui sorter = new SimpleGui(n);
-        
+        mainframe.setVisible(false);
     }
-    
+
     public static void main() {
-        
+
         SortDemo sorting = new SortDemo();
 
     }
