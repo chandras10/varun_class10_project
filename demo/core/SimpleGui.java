57d0b125d0a1a45c4e37a90a8d08c8f59bf72e07
chandras10 <chandras10>
2018-11-15 12:57:50 +0530

    animation almost finalized

--- demo/core/SimpleGui.java
+++ demo/core/SimpleGui.java
@@ -13,26 +13,23 @@ public class SimpleGui extends JPanel implements ActionListener{
     JFrame m_frame;
     JButton m_button;
     int m_array[];
+    boolean m_state[];
     
     public SimpleGui(int i) {
         m_frame = new JFrame();
-        //this.add(m_frame);
         m_button = new JButton("Sort the array");
         m_array = new int[i];
+        m_state = new boolean[i];
+        
         for(int n = 0; n < m_array.length; n++) {
             m_array[n] = n;
+            m_state[n] = false;
         }
+        
         m_button.addActionListener(this);
        
         m_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.shuffle(m_array);
-
-        JScrollPane scrollPane = new JScrollPane(this,
-            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
-            JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        //scrollPane.setPreferredScrollableViewportSize(500, 500);
-        m_frame.add(scrollPane);
-        m_frame.getContentPane().add(scrollPane);
         m_frame.getContentPane().add(BorderLayout.CENTER,this);
         m_frame.getContentPane().add(BorderLayout.NORTH,m_button);
         m_frame.setSize(500, 500);
@@ -56,23 +53,28 @@ public class SimpleGui extends JPanel implements ActionListener{
 
     public void paintComponent(Graphics g) {
             g.fillRect(0, 0, this.getWidth(), this.getHeight());
-            g.setColor(Color.orange);
+            
             int j, k = 0;
             int w = this.getHeight() - 10;
             int barWidth = Math.max((m_frame.getWidth()/m_array.length), 5);
             int barHeight = Math.min((m_frame.getHeight()/m_array.length), 10);
             
             for (int a = 0; a < m_array.length; a++) {
+                
+                if(m_state[a] == true)
+                    g.setColor(Color.red);
+                else
+                    g.setColor(Color.orange);
                 j = (m_array[a]+1) * barHeight; //Scaling the array value to make the bars visible
                 g.fillRect(k, w - j, barWidth, j);
                 k += barWidth + 5;
+                
             }
             
     }    
         
     public void actionPerformed(ActionEvent event) {
 
-        int temp;
         int j = 0;
         int length = m_array.length;
         for (int i = 0; i < length; i++) {
@@ -81,9 +83,11 @@ public class SimpleGui extends JPanel implements ActionListener{
                 
                 if (m_array[j] < m_array[minIndex])
                     minIndex = j;
+                    
+                m_state[minIndex] = m_state[i] = true;
             }
 
-            temp = m_array[minIndex];
+            int temp = m_array[minIndex];
             m_array[minIndex] = m_array[i];
             m_array[i] = temp;
             
@@ -94,6 +98,7 @@ public class SimpleGui extends JPanel implements ActionListener{
             }
             
             this.paintImmediately(0, 0, m_frame.getWidth(), m_frame.getHeight());
+            m_state[minIndex] = m_state[i] = false;
         }
         
         
