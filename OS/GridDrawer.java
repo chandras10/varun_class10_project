fcae9dc4637d6a39b2d430422cfd936eb88df44a
chandras10 <chandras10>
2018-12-29 19:46:53 +0530

    grid made relative to frame size in battle game

--- OS/GridDrawer.java
+++ OS/GridDrawer.java
@@ -1,6 +1,5 @@
 package OS;
 
- 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -11,13 +10,18 @@ public class GridDrawer extends JPanel{
     JFrame frame;
     int columns = 7;
     int rows = 7;
-    
+
     boolean state[][] = new boolean[columns][rows];
 
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
     public void setUpGrid() {
-        
+
         frame = new JFrame();  
-        frame.setSize(400, 400);
+        // setting the frame size to fill the screen
+
+        frame.setSize(500, 500);
+        //frame.setSize(screenSize.width, screenSize.height);
 
         for (int i = 0; i < columns; i++) {
             for (int j = 0; j < rows; j++) {
@@ -30,7 +34,7 @@ public class GridDrawer extends JPanel{
     }
 
     public void setState(boolean theState, String theGuess) {
-        
+
         char ch = theGuess.charAt(0);
         char num = theGuess.charAt(1);
 
@@ -42,19 +46,19 @@ public class GridDrawer extends JPanel{
                 state[c][n] = theState;
             }
         }
-        
+
         this.repaint();
-        
+
     }
-    
+
     public void paintComponent (Graphics g) {
-        
+
         g.setColor(Color.black);
         int width = this.getWidth();
         int height = this.getHeight();
 
         int gap = 0;
-        int circleWidth = 30;
+        int circleWidth = (width / 9) / 2;
         int YGap = 0;
 
         int numbersX[] = {0, 1, 2, 3, 4, 5, 6};
@@ -73,18 +77,20 @@ public class GridDrawer extends JPanel{
                     g.setColor(Color.orange);
                 }
 
-                g.fillOval((numbersX[i] + 1 * 10) + gap, (((numbersX[j] + 1) * 10)) + YGap, circleWidth, 30);
-                gap += 50;
+                g.fillOval(width/100 + gap, height/100 + YGap, circleWidth, circleWidth);
+                gap += width/7;
             }
 
-            g.drawString(alphabets[j], (numbersX[j] + 1 * 10) + gap, ((numbersX[j] + 1) * 10) + YGap + 20);
-            YGap += 40;
+            gap = (width/7) * 6;
+            
+            g.drawString(alphabets[j], (width/100) + gap + (circleWidth*2), height/100 + YGap + (circleWidth/2));
+            YGap += (height/7);
             gap = 0;
         }
-
+        
         for (int k = 0; k < numbersX.length; k ++) {
-            gap += 50;
-            g.drawString(Integer.toString(numbersX[k]),(numbersX[k] + 1 * 10) + gap - 40, YGap + 80);
+            gap += width/7;
+            g.drawString(Integer.toString(numbersX[k]),width/100 + gap - (circleWidth*2), height/100 + YGap - circleWidth/4);
         }
     }
 
